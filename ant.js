6dc531aa2496742e254919eef52b7c7c016420af
Joshua T Corbin <jcorbin@wunjo.org>
2011-02-18 00:29:14 -0500

    Initial crack

---
+++ ant.js
@@ -0,0 +1,59 @@
+if (typeof(Ants) == 'undefined')
+    Ants = {};
+
+Ants.Ant = (function() {
+    var Right = 1;
+    var Left = 0;
+    var DefaultTurns = "RL";
+
+    function parseTurnDirections(s) {
+        var d = [];
+        s = s.toLowerCase();
+        for (var i=0; i<s.length; i++) {
+            var c = s[i];
+            if (c == 'r')
+                d.push(Right);
+            else if (c == 'l')
+                d.push(Left);
+            else
+                throw Error('Invalid turn direction');
+        }
+        return d;
+    }
+
+    function Ant(row, col, heading, color, turns) {
+        this.row = row || -1;
+        this.col = col || -1;
+        this.heading = heading || 0;
+        this.color = color || '#f00';
+        this.turns = parseTurnDirections(turns || DefaultTurns);
+    }
+
+    Ant.prototype.draw = function() {
+        // TODO: oriented shape
+        var ctx = this.grid.canvas.getContext('2d');
+        ctx.fillStyle = this.color;
+        ctx.fillRect(this.row+0.25, this.col+0.25, 0.5, 0.5);
+    };
+
+    var HalfPI = Math.PI/2;
+    var TwoPI = 2*Math.PI;
+
+    Ant.prototype.step = function() {
+        var c = this.grid.data[this.row][this.col];
+        var t = this.turns[c];
+        switch (t) {
+            case Right:
+                this.heading = (this.heading + HalfPI) % TwoPI;
+                break;
+            case Left:
+                this.heading = (this.heading - HalfPI) % TwoPI;
+                break;
+        }
+        this.grid.data[this.row][this.col] = (c + 1) % this.grid.colors.length;
+        this.col += Math.round(Math.cos(this.heading));
+        this.row += Math.round(Math.sin(this.heading));
+    };
+
+    return Ant;
+})();
---
+++ grid.js
@@ -0,0 +1,108 @@
+if (typeof(Ants) == 'undefined')
+    Ants = {};
+
+Ants.Grid = (function() {
+    var DefaultColors = ['#fff', '#000'];
+    var DefaultRows = 50;
+    var DefaultCols = 50;
+
+    function Grid(canvas, rows, cols, colors) {
+        this.colors = colors || DefaultColors;
+        this.canvas = canvas;
+        this.ants = [];
+        this.setSize(rows || DefaultRows, cols || DefaultCols);
+
+        var me = this;
+        window.addEventListener('resize', function() {me.updateSize()});
+    }
+
+    Grid.prototype.setSize = function(rows, cols) {
+        this.rows = rows;
+        this.cols = cols;
+
+        // TODO: non-destructive size-change
+        this.data = [];
+        for (var i=0; i<this.rows; i++) {
+            var row = [];
+            for (var j=0; j<this.cols; j++)
+                row.push(0);
+            this.data.push(row);
+        }
+
+        // TODO: out of bound ants
+
+        this.updateSize();
+    };
+
+    Grid.prototype.updateSize = function() {
+        this.scale = Math.min(
+            this.canvas.parentElement.clientWidth/this.cols,
+            this.canvas.parentElement.clientHeight/this.rows
+        );
+
+        this.canvas.width = this.cols * this.scale;
+        this.canvas.height = this.rows * this.scale;
+        var ctx = this.canvas.getContext('2d');
+        ctx.scale(this.scale, this.scale);
+
+        this.render();
+    };
+
+    Grid.prototype.randomize = function() {
+        var n_colors = this.colors.length;
+        for (var i=0; i<this.rows; i++)
+            for (var j=0; j<this.cols; j++)
+                this.data[i][j] = Math.floor(Math.random() * n_colors);
+    };
+
+    Grid.prototype.drawCell = function(row, col) {
+        var ctx = this.canvas.getContext('2d');
+        ctx.fillStyle = this.colors[this.data[row][col]];
+        ctx.fillRect(row, col, 1, 1);
+    };
+
+    Grid.prototype.render = function() {
+        var ctx = this.canvas.getContext('2d');
+
+        // Flood fill color 0
+        ctx.fillStyle = this.colors[0];
+        ctx.fillRect(0, 0, this.cols, this.rows);
+
+        // TODO does it pay to aggregate draws by fill color?
+        for (var i=0; i<this.rows; i++)
+            for (var j=0; j<this.cols; j++)
+                if (this.data[i][j] != 0) // already flood filled 0
+                    this.drawCell(i, j);
+
+        for (var i=0; i<this.ants.length; i++)
+            this.ants[i].draw();
+    };
+
+    Grid.prototype.addAnt = function(ant) {
+        if (! this.isInBounds(ant.row, ant.col)) {
+            // Place ants on a spiral initially
+            var t = (this.ants.length+1);
+            ant.row = Math.floor((t * Math.sin(t) + 50)/100 * this.rows);
+            ant.col = Math.floor((t * Math.cos(t) + 50)/100 * this.cols);
+        }
+        this.ants.push(ant);
+        ant.grid = this;
+        ant.draw();
+    };
+
+    Grid.prototype.isInBounds = function(row, col) {
+        return row >= 0 && col >= 0 && row <= this.rows && col <= this.cols;
+    };
+
+    Grid.prototype.step = function() {
+        for (var i=0; i<this.ants.length; i++) {
+            var ant = this.ants[i];
+            var old_row = ant.row, old_col = ant.col;
+            ant.step();
+            this.drawCell(old_row, old_col);
+            ant.draw();
+        }
+    };
+
+    return Grid;
+})();
